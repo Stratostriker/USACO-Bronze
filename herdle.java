1b16c2570a0d1fe3367d8fc32533b9d8501135da
Alan Gu <57688846+Stratostriker@users.noreply.github.com>
2022-02-27 12:55:47 -0500

    Update and rename problem1.java to herdle.java

--- herdle.java
+++ herdle.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-public class problem1 {
+public class herdle {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         PrintWriter pw = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
